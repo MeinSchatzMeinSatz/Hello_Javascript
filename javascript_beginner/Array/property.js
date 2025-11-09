5246ac7a64a759de88d8d3bbb7007c8dcdf99a97
Liebe_Chaeso <d131215@naver.com>
2025-11-09 11:27:48 +0900

    array object

---
+++ javascript_beginner/Array/property.js
@@ -0,0 +1,23 @@
+// Array 오브젝트 프로퍼티(ES3)
+/** ES3 프로퍼티 리스트
+ * new Array()
+ * Array()
+ *
+ * Array 프로퍼티
+ * length
+ *
+ * Array prototype
+ * contructor
+ * unshift()
+ * push()
+ * concat()
+ * slice()
+ * join()
+ * toString()
+ * toLocaleString()
+ * shift()
+ * pop()
+ * splice()
+ * sort() : 엘리먼트 값을 Unicode 순서로 분류하여 반환
+ * reverse() : 엘리먼트 '위치'를 역순으로 바꾼다.
+ */
